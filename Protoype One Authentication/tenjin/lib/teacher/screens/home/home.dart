5019365a3eae53130386191f0cf955c9d49f3fbb
laurij95 <laureljackson74@gmail.com>
2020-03-28 15:42:53 -0400

    Prototype One
    This prototype deals with authenticating users by allowing users to create accounts and logging in. This authentication is functional due to Firebase.

---
+++ Protoype One Authentication/tenjin/lib/teacher/screens/home/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tenjin/teacher/service/auth.dart';
+
+class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[50],
+      appBar: AppBar(
+        title: Text('Tenjin'),
+        backgroundColor: Colors.amber[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Logout'),
+            onPressed: () async{
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
